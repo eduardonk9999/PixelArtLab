a52b035872cf7d5bf86de27ab244203f77c81aa0
eduardonk9999 <thiago.novak7@gmail.com>
2025-12-26 17:37:47 -0300

    logica, de preencher e apagar os blocos

--- pixelartlab.js
+++ pixelartlab.js
@@ -4,12 +4,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let currentColor = "black"
 
+    const activate = (event) => {
+        if (event.target.style.backgroundColor && event.target.style.backgroundColor !== 'transparent') {
+            event.target.style.backgroundColor = 'transparent'
+        } else {
+            event.target.style.backgroundColor = currentColor
+        }
+    }
+
+    const changeColor = (event) => {
+        currentColor = event.target.attributes["data-color"].value;
+    }
+
+    colors.forEach(color => {
+        color.addEventListener("click", changeColor)
+    })
+
     for (let i = 0; i < 24 * 24; i++) {
         const cell = document.createElement("div")
         cell.classList.add("cell")
 
         gridElement.appendChild(cell)
     }
+
+    const cells = document.querySelectorAll(".cell")
+
+    cells.forEach(cell => {
+        cell.addEventListener("click", activate);
+    })
 
 })
 
